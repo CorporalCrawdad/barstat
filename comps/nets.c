a72494fd8a38a2f69567bef16f9c53f560de5042
Robert Stancil <robert.stancil@live.com>
2018-03-03 10:01:41 -0600

    (incomplete) progress on net component

--- comps/nets.c
+++ comps/nets.c
@@ -5,17 +5,18 @@
 #define BUF_SZ 1024
 #define HIST_CNT 5
 
-static void net_init(void);
-void net_dest(void);
-const char* getnets(void);
+static void  net_init(void);
+static void  net_updt(void);
+       void  net_dest(void);
+const  char *getnets(void);
 
 static char netsbuffer[ BUF_SZ ];
 static int *ulhist[ HIST_CNT ];
 static int *dlhist[ HIST_CNT ];
-static int histit = -1;
+static int  histit = -1;
 
 static void
-net_init()
+net_init(void)
 {
 	for (int i=0; i<sizeof(netsbuffer); i++)
 		netsbuffer[i] = 0;
@@ -27,4 +28,60 @@ net_init()
 	atexit(net_dest);
 }
 
+void
+net_dest(void)
+{
+	for (int i=0; i<HIST_CNT; i++) {
+		free(ulhist[i]);
+		free(dlhist[i]);
+	}
+}
 
+// modes: 0 for dl, 1 for ul
+const char *
+getnets(int upload)
+{
+	int avg = 0;
+	int *histp = NULL;
+
+	if (histit < 0) {
+		net_init();
+	}
+
+	// update usage arrays
+	net_updt();
+
+	// array is not full, can't process avg yet
+	if (histit+1 < HIST_CNT) {
+		return bprintf("0");
+	}
+
+	if (upload)
+		histp = ulhist;
+	else
+		histp = dlhist;
+
+	// get total differences between each data point
+	for (int i=1; i<HIST_CNT; i++) {
+		avg += *histp[i] - *histp[i-1];
+	}
+	// average them out and return the result
+	return bprintf("%i", (avg / (HIST_CNT-1)));
+}
+
+static void
+net_updt(const char *net_pref)
+{
+	FILE* procnetdev = fopen("/proc/net/dev", "r");
+	int ret = 0;
+	char cbuf = '\0';
+	int netul, netdl;
+	char netsbuffer[CHAR_BUF];
+
+	if (procnetdev == NULL)
+		return -1;
+	
+	// scroll through /proc/net/dev to interface
+	while (strncmp(netsbuffer, net_pref, 3))
+		for (int i=0; i<CHAR_BUF; i++) {
+			netsbuffer[i] = fgetc (procnetdev);
--- makefile
+++ makefile
@@ -1,17 +1,29 @@
-status : status.o 
-	gcc status.o batt.o -o status -lasound -lmpdclient -lX11
+status : status.o batt.o
+	gcc status.o comps/batt.o comps/mopd.o comps/nets.o comps/time.o comps/volm.o -o status -lasound -lmpdclient -lX11
 
 status.o : status.c status.h config.h bat.h
 	gcc -c status.c
 
-bat.h : configure
+comps/bat.h : configure
 	./configure
 
-batt.o : batt.c bat.h
+comps/batt.o : batt.c bat.h
 	gcc -c batt.c
+
+comps/mopd.o : mopd.c
+
+
+comps/nets.o : nets.c
+
+
+comps/time.o : time.c
+
+
+comps/volm.o : volm.c
+
 
 config.h : config.def.h
 	cp config.def.h config.h
 
 clean : 
-	rm *.o status bat.h
+	rm *.o */*.o status comps/bat.h
