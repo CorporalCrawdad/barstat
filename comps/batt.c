05c7b3fa08dc496918cc4df30303a1efba679fb7
Robert Stancil <robert.stancil@mavs.uta.edu>
2018-03-06 00:42:59 -0600

    fixed batt.c no battery case

--- comps/batt.c
+++ comps/batt.c
@@ -7,11 +7,14 @@ getbatlvl(void)
 {
 	int chnow, chfull;
 
-	pscanf(BT_NOW, "%i", &chnow);
-	pscanf(BT_FULL, "%i", &chfull);
+	if (BT_NOW)
+		if (BT_FULL) {
+			pscanf(BT_NOW, "%i", &chnow);
+			pscanf(BT_FULL, "%i", &chfull);
+		}
 
 	if (chnow)
 		if (chfull)
 			return bprintf("%i", ((chnow*100)/chfull));
-	return NULL;
+	return bprintf("%i", 0);
 }
